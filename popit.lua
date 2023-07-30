dab524b42663ababa0f3b1fef4768f89777f4e43
HUNTScripting <126942763+HUNTScripting@users.noreply.github.com>
2023-07-30 14:36:09 +0300

    Create popit.lua

---
+++ popit.lua
@@ -0,0 +1,79 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TweenService = game:GetService("TweenService")
+local RunService = game:GetService("RunService")
+local Player = game.Players.LocalPlayer
+local Library = (loadstring(game:HttpGet("https://raw.githubusercontent.com/HUNTScripting/scripts/main/newui.lua")))()
+local Stats = ReplicatedStorage.Stats[Player.Name]
+local Main = Library:AddSection("🔥 Autofarm")
+local AreaPath = require(ReplicatedStorage.Tables.AreaWorldData)
+local Areas = {}
+for i,v in pairs(AreaPath.areas) do
+    table.insert(Areas, tostring(i))
+end
+for i = 1, math.floor(#Areas/2) do
+    local j = #Areas - i + 1
+     Areas[i], Areas[j] = Areas[j], Areas[i]
+ end
+local SelectedArea = nil
+local function GetPlayerPets()
+	local pets = {}
+	for i, v in pairs(Stats.Pets:GetChildren()) do
+		if v.Equipped.Value then
+			table.insert(pets, v.Name)
+		end
+	end
+	return pets
+end
+local function CheckIfPetsTarget(action)
+    local pets = GetPlayerPets()
+    for i,v in pairs(pets) do
+        local workspacePet = workspace.Pets:FindFirstChild(v)
+        if not workspacePet then
+            continue
+        end
+        local hasTarget = workspacePet:GetAttribute("target") == "player"
+        if hasTarget then 
+            action(v)
+        end
+    end
+end
+Main:AddDropdown("Select your Area: ", Areas, function(value)
+	SelectedArea = value
+end)
+local isAutoPopOn
+local popRemote = ReplicatedStorage["SSG Framework"].Shared.Network["attack drop"]
+Main:AddToggle("AutoFarm", "AutoFarm selected areas ", function(yeet)
+	isAutoPopOn = yeet
+	while isAutoPopOn do
+		for _, Ballon in pairs(workspace.Drops:GetChildren()) do
+			local Area = Ballon:GetAttribute("Area")
+			if Area == SelectedArea then
+				while Ballon:GetAttribute("HP") > 0 and isAutoPopOn do
+                    CheckIfPetsTarget(function(petName)
+					    popRemote:InvokeServer(SelectedArea, petName, Ballon.Name, true, Vector3.new(472, 26, -425))
+                    end)
+					task.wait(0.1)
+				end
+			end
+		end
+		task.wait(0.1)
+	end
+end)
+local isAutoEquipPetsOn;
+local autoEquipRemote = ReplicatedStorage["SSG Framework"].Shared.Network["equip best pets"]
+local function runAutoEquip()
+    if isAutoEquipPetsOn then
+        task.wait(3)
+        autoEquipRemote:InvokeServer()
+    end
+end
+Stats.Pets.ChildAdded:Connect(runAutoEquip)
+Stats.Pets.ChildRemoved:Connect(runAutoEquip)
+
+Main:AddToggle("Auto Equip Pets", "Auto equips best pets for you", function(yeet)
+    isAutoEquipPetsOn = yeet
+    runAutoEquip()
+end)
+Main:AddButton("Destroy the gui", function()
+	Library:Destroy()
+end)
